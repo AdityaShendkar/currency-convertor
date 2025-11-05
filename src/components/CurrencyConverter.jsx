9295fab7f4ad3f77f86ae6b41a51e0db66f2ae8c
Aditya Shendkar <aditya.shendkar03@gmail.com>
2025-11-05 14:35:47 +0530

    feat: implement CurrencyDropDown component and enhance CurrencyConverter layout

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,9 +1,79 @@
+import { useState, useEffect } from "react";
+import CurrencyDropDown from "./CurrencyDropDown";
+
 const CurrencyConverter = () => {
+  const [currencies, setCurrencies] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setfromCurrency] = useState("USD")
+  const [toCurrency, settoCurrency] = useState("INR")
+
   // AllCurrencies => https://api.frankfurter.dev/v1/currencies
+
+  const fetchCurrencies = async () => {
+    try {
+      const res = await fetch("https://api.frankfurter.dev/v1/currencies");
+      const data = await res.json();
+      setCurrencies(Object.keys(data));
+      // console.log(data);
+    } catch (error) {
+      console.error("Fetching error", error);
+    }
+  };
+  
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
+  // console.log(currencies);
+
+  const convertCurrency = () => {
+
+
+  }
+  
+
   // Conversion =>https://api.frankfurter.dev/v1/latest?base=usd&symbols=inr
-  return <div>
-    <h2>Currency Converter</h2>
-  </div>;
+  return (
+    <div className=" max-w-2xl  mx-auto my-10 bg-white rounded-md px-4 py-4 shadow-md ">
+      <h2 className=" mb-5 text-3xl font-medium text-gray-700 ">
+        Currency Converter
+      </h2>
+
+      <div>
+        <CurrencyDropDown currencies={currencies}  title="From: "/>
+        {/* Button swap */}
+        <CurrencyDropDown currencies={currencies} title="To: "/>
+      </div>
+
+      <div>
+        <label
+          htmlFor="amount"
+          className=" block text-sm font-medium text-gray-700 "
+        >
+          Amount:
+        </label>
+        <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          type="number"
+          name=""
+          id=""
+          className=" w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1 "
+        />
+      </div>
+      <div 
+      className="flex justify-end mt-6">
+        <button
+          onClick={convertCurrency}
+          className={`px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+        >
+          Convert
+        </button>
+      </div>
+      <div className=" mt-4 text-lg font-medium text-center text-green-600">
+        Converted Amount: 1000USD
+      </div>
+    </div>
+  );
 };
 
 export default CurrencyConverter;
---
+++ src/components/CurrencyDropDown.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const CurrencyDropDown = ({
+  currencies,
+  currency,
+  setCurrency,
+  favorites,
+  handleFavorite,
+  title = "",
+}) => {
+  return (
+    <div>
+      <label
+        htmlFor={title}
+        className="block text-sm font-medium text-gray-700"
+      >
+        {title}
+      </label>
+
+      <div className="mt-1 relative">
+        <select
+          name=""
+          id=""
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+          {currencies.map((currency) => {
+            return (
+              <option value="currency" key={currency}>
+                {currency}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyDropDown;
