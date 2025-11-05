dbba33d1d56d8dea4b3a9534a1f50c4bd075803b
Aditya Shendkar <aditya.shendkar03@gmail.com>
2025-11-05 12:45:58 +0530

    fix: correct spelling of CurrencyConverter and remove obsolete CurrencyConvertor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import CurrencyConverter from "./components/CurrencyConverter";
+
 function App() {
   return (
     <div className=" min-h-screen bg-gray-200 flex justify-center items-center flex-col ">
-      <CurrencyConvertor />
+      <CurrencyConverter />
     </div>
   );
 }
---
+++ src/components/CurrencyConverter.jsx
@@ -0,0 +1,9 @@
+const CurrencyConverter = () => {
+  // AllCurrencies => https://api.frankfurter.dev/v1/currencies
+  // Conversion =>https://api.frankfurter.dev/v1/latest?base=usd&symbols=inr
+  return <div>
+    <h2>Currency Converter</h2>
+  </div>;
+};
+
+export default CurrencyConvertor;
